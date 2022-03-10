951414a4b061849d8adac4007fca4b140d9c76cf
anudeepreddy <anudeepc85@gmail.com>
2022-03-10 19:02:57 +0530

    mini-miner solved

--- mini-miner/index.js
+++ mini-miner/index.js
@@ -1,29 +1,69 @@
-let payload = {
-  "block": {
-    "data": [
-      ["d26b9b5c4329da62271039e30784a26a", -18],
-      ["212079f594a489b2dad0f13c13867246", -59],
-      ["8e47c8f755a134bc21e8ded74ec58d71", 65],
-      ["f3d73e853d23719b61ac1c17b22f82a2", -69]
-    ],
-    "nonce": null
-  },
-  "difficulty": 14
-};
-const crypto = require('crypto');
-console.log(payload)
-let n = 89174867;
-let difficultyBits = '0'.repeat(payload.difficulty);
+import crypto from "crypto";
+import cluster from "cluster";
+import { cpus } from "os";
+import process from "process";
+import fs from "fs";
 
-while(true){
-  payload.block.nonce = n;
-  let hashInHex = crypto.createHash('sha256').update(JSON.stringify(payload.block)).digest('hex');
-  let binaryHash = parseInt(hashInHex, 16).toString('2');
-  let computedDiffBits = binaryHash.slice(0, payload.difficulty);
-  console.log(computedDiffBits,":",n);
-  if(computedDiffBits === difficultyBits){
-    console.log("Nonce:",n);
-    break;
+let payload = {
+  block: {
+    data: [
+      ["55f76f1deb50a49b35ab5c1d9463eb89", -39],
+      ["8307669500017536a80363dfcf9581ce", 84],
+      ["8e9bf78716171f24fbc1612343701124", 68],
+      ["a98a9ddb383fa68c1385b19d50b01aef", -25],
+      ["e7190e18bc7824c0fea1f513187fff6e", -41],
+    ],
+    nonce: null
+  },
+  difficulty: 13
+};
+
+let difficultyBits = "0".repeat(payload.difficulty);
+let cpuCount = cpus().length;
+
+if (cluster.isMaster) {
+  console.log(`Primary ${process.pid} is running`);
+
+  // Fork workers.
+  for (let i = 1; i <= cpuCount; i++) {
+    console.log("called");
+    let newWorker = cluster.fork({ startIndex: i });
   }
-  n++;
+
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  let n = parseInt(process.env.startIndex);
+  // console.log();
+  while (true) {
+    payload.block.nonce = n;
+    let hashInHex = crypto
+      .createHash("sha256")
+      .update(JSON.stringify(payload.block))
+      .digest("hex");
+    let binaryHash = parseInt(hashInHex, 16).toString("2");
+    let computedDiffBits = binaryHash.slice(0, payload.difficulty);
+    console.log(
+      computedDiffBits,
+      ":",
+      n,
+      ":",
+      binaryHash.length,
+      ":",
+      256 - binaryHash.length
+    );
+    if (
+      computedDiffBits === difficultyBits ||
+      256 - binaryHash.length >= payload.difficulty
+    ) {
+      console.log("Nonce:", n);
+      fs.writeFileSync(
+        `nonce${n}.txt`,
+        `nonce:${n}\nbhash:${binaryHash}\nhhash:${hashInHex}`
+      );
+      break;
+    }
+    n = n + cpuCount;
+  }
 }
